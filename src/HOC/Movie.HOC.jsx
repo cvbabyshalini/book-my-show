e9523581ddffc94955bc8dd9240f58961589f24f
Baby Shalini <chowlababy@gmail.com>
2021-07-16 15:56:15 +0530

    add reusable components

---
+++ src/HOC/Movie.HOC.jsx
@@ -0,0 +1,21 @@
+import { Route } from "react-router-dom";
+
+//Layout
+import MovieLayout from "../layouts/Movie.layout";
+
+const MovieHOC = ({ component: Component, ...rest }) => {
+    return (
+        <>
+            <Route
+                {...rest}
+                component={(props) => (
+                    <MovieLayout>
+                        <Component {...props} />
+                    </MovieLayout>
+                )}
+            />
+        </>
+    );
+};
+
+export default MovieHOC;
---
+++ src/pages/Movie.page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Movie= () => {
+    return (
+        <>
+            Hi
+        </>
+    )
+};
+
+export default Movie;
